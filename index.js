09068198955a730d94a661f9949d443a2997b3d7
Anthony Moreno <tonymoreno86@gmail.com>
2019-03-27 13:36:14 -0700

    Updated name from dice.html to index.html

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
 
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!🚩";
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 } else if ( randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+  document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
 } else {
   document.querySelector("h1").innerHTML = "Draw! 😲";
 }
